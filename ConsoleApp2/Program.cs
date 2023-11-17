60c8a51909ddc0dfd0f0ab7c75723a61d26fc65f
Administrator <Administrator@WIN-QH3366O2A51>
2023-11-17 22:33:29 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿//3.3 Высокий уровень #2
+
+try{
+    Console.Write(" Введите x: ");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write(" Введите n: ");
+    int n = int.Parse(Console.ReadLine());
+    double S = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0){
+            S += Math.Cos(i * x) / i; }
+        else{
+            S += Math.Cos(i * x) / i; }
+    }
+    Console.WriteLine($"S={S:F2}");
+}
+catch (Exception err){
+    Console.WriteLine(err); }
